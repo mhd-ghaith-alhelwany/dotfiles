bf536d8112c895cc0bab76f6eceb75571a2566a2
Mhd Ghaith Alhelwany <mhdghaithalhelwany@gmail.com>
2021-12-24 18:20:26 -0600

    fix issues

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
-sudo apt-get purge --auto-remove gnome-terminal ubuntu-gnome-desktop gnome-shell gdm nautilus gnome-help
 sudo apt-get install i3-gaps i3lock-fancy feh xss-lock rofi kitty fonts-font-awesome light i3blocks thunar blueman lxappearance nodm 
+sudo apt-get purge --auto-remove gnome-terminal ubuntu-gnome-desktop gnome-shell gdm nautilus gnome-help
 sudo apt-get autoremove 
-sudo cp ../.config/* $HOME/.config
-sudo cp ../.themes/* $HOME/.themes
-sudo cp ../.icons/* $HOME/.icons
+sudo cp ../.config/* $HOME/.config -r
+sudo cp ../.themes/* $HOME/.themes -r
+sudo cp ../.icons/* $HOME/.icons -r
