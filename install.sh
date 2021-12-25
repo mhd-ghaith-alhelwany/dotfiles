bddfab6fb3e269e1f4a927f5ccc3edd2ea50bbc3
Mhd Ghaith Alhelwany <mhdghaithalhelwany@gmail.com>
2021-12-24 18:53:41 -0600

    fix -y issue

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 sudo add-apt-repository ppa:regolith-linux/release -y
 sudo apt update
-sudo apt-get install i3-gaps i3lock-fancy feh xss-lock rofi kitty fonts-font-awesome light i3blocks thunar blueman lxappearance-y
+sudo apt-get install i3-gaps i3lock-fancy feh xss-lock rofi kitty fonts-font-awesome light i3blocks thunar blueman lxappearance -y
 # dpkg --remove gnome-terminal ubuntu-gnome-desktop gnome-shell gdm nautilus -y
 sudo apt-get autoremove 
 sudo cp dotfiles/.config/* $HOME/.config -r
