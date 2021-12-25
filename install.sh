911bdf33d562934db2e51ebb54923d497d136563
Mhd Ghaith Alhelwany <mhdghaithalhelwany@gmail.com>
2021-12-24 18:36:28 -0600

    fix package removing issues

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 sudo add-apt-repository ppa:regolith-linux/release -y
 sudo apt update
 sudo apt-get install i3-gaps i3lock-fancy feh xss-lock rofi kitty fonts-font-awesome light i3blocks thunar blueman lxappearance nodm -y
-sudo apt-get purge --auto-remove gnome-terminal ubuntu-gnome-desktop gnome-shell gdm nautilus gnome-help
+dpkg --remove gnome-terminal ubuntu-gnome-desktop gnome-shell gdm nautilus -y
 sudo apt-get autoremove 
 sudo cp dotfiles/.config/* $HOME/.config -r
-sudo cp dotfiles/.themes/* $HOME/.themes -r
+sudo cp dotfiles/.fonts/* $HOME/.fonts -r
 sudo cp dotfiles/.icons/* $HOME/.icons -r
