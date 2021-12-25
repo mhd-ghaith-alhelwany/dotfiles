e4ef363a14d2db9bab105fb5ba9c49c194d2de2f
Mhd Ghaith Alhelwany <mhdghaithalhelwany@gmail.com>
2021-12-24 18:26:38 -0600

    fix i3-gaps repo issue

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/bash
-echo "installing apps"
+sudo add-apt-repository ppa:regolith-linux/release
+sudo apt update
+sudo apt install i3-gaps
+
 sudo apt-get install i3-gaps i3lock-fancy feh xss-lock rofi kitty fonts-font-awesome light i3blocks thunar blueman lxappearance nodm 
-echo "uninstalling apps"
 sudo apt-get purge --auto-remove gnome-terminal ubuntu-gnome-desktop gnome-shell gdm nautilus gnome-help
 sudo apt-get autoremove 
 sudo cp ../.config/* $HOME/.config -r
