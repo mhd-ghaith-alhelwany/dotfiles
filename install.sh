0f601536a2b955b5a01212e8c9ada36902b4e0d2
Mhd Ghaith Alhelwany <mhdghaithalhelwany@gmail.com>
2021-12-24 18:32:47 -0600

    fix directory issue

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
-sudo add-apt-repository ppa:regolith-linux/release
+sudo add-apt-repository ppa:regolith-linux/release -y
 sudo apt update
 sudo apt-get install i3-gaps i3lock-fancy feh xss-lock rofi kitty fonts-font-awesome light i3blocks thunar blueman lxappearance nodm -y
 sudo apt-get purge --auto-remove gnome-terminal ubuntu-gnome-desktop gnome-shell gdm nautilus gnome-help
 sudo apt-get autoremove 
-sudo cp .config/* $HOME/.config -r
-sudo cp .themes/* $HOME/.themes -r
-sudo cp .icons/* $HOME/.icons -r
+sudo cp dotfiles/.config/* $HOME/.config -r
+sudo cp dotfiles/.themes/* $HOME/.themes -r
+sudo cp dotfiles/.icons/* $HOME/.icons -r
