c479667971469e201082f05d399caf1347f3e99d
Mhd Ghaith Alhelwany <mhdghaithalhelwany@gmail.com>
2021-12-24 19:01:23 -0600

    fix rofi theme

--- install.sh
+++ install.sh
@@ -4,4 +4,6 @@ sudo apt update
 sudo apt-get install i3-gaps i3lock-fancy feh xss-lock rofi kitty fonts-font-awesome light i3blocks thunar blueman lxappearance -y
 # dpkg --remove gnome-terminal ubuntu-gnome-desktop gnome-shell gdm nautilus -y
 sudo apt-get autoremove 
-sudo cp dotfiles/* $HOME -r
+sudo cp dotfiles/.config/* $HOME/.config -r
+sudo cp dotfiles/.fonts/* $HOME/.fonts -r
+sudo cp dotfiles/.icons/* $HOME/.icons -r
