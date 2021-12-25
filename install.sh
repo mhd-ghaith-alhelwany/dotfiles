5023fcf1b58f6841d39a081e12644b8892d39964
Mhd Ghaith Alhelwany <mhdghaithalhelwany@gmail.com>
2021-12-24 19:07:10 -0600

    fix removed packages

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 sudo add-apt-repository ppa:regolith-linux/release -y
 sudo apt update
 sudo apt-get install i3-gaps i3lock-fancy feh xss-lock rofi kitty fonts-font-awesome light i3blocks thunar blueman lxappearance -y
-# dpkg --remove gnome-terminal ubuntu-gnome-desktop gnome-shell gdm nautilus -y
+dpkg --remove gnome-terminal ubuntu-gnome-desktop nautilus -y
 sudo apt-get autoremove 
 sudo cp dotfiles/.config/* $HOME/.config -r
 sudo cp dotfiles/.fonts/* $HOME/.fonts -r
