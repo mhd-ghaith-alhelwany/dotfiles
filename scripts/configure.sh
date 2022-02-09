d2262149512184c3158d7ef7091980d7ae820724
Mhd Ghaith Alhelwany <mhdghaithalhelwany@gmail.com>
2022-02-09 17:30:01 -0500

    add extract script

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -3,5 +3,6 @@ sudo cp dotfiles/.fonts $HOME/ -r
 sudo cp dotfiles/.icons $HOME/ -r
 sudo cp dotfiles/.wallpapers $HOME -r
 sudo cp dotfiles/.scripts $HOME -r
+sudo cp dotfiles/.bash_aliases $HOME
 sudo chown $USER $HOME/* -R
 sudo chmod +x $HOME/.scripts/*
