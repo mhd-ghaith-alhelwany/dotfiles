9a4945871f978b0ed8063540a19ebc31016662b1
Mhd Ghaith Alhelwany <mhdghaithalhelwany@gmail.com>
2022-02-18 11:26:19 -0500

    disable snap installs

--- scripts/install.sh
+++ scripts/install.sh
@@ -12,13 +12,13 @@ do
     fi
 done
 
-snap_packages=( vlc motrix )
-for i in "${snap_packages[@]}"
-do
-if [[ $(snap list | grep $i) ]]; then
-    echo "$i was found. skipping"
-else
-    echo "installing $i"
-    sudo snap install "$i" > /dev/null
-fi
-done
+# snap_packages=( vlc motrix )
+# for i in "${snap_packages[@]}"
+# do
+# if [[ $(snap list | grep $i) ]]; then
+#     echo "$i was found. skipping"
+# else
+#     echo "installing $i"
+#     sudo snap install "$i" > /dev/null
+# fi
+# done
